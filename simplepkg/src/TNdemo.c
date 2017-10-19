df8ae62d9437393c90bdb0fcc7d929c2fd0bfc92
Ulbricht, Marian <ulbricht@innoroute.de>
2017-10-19 10:46:15 +0200

    addsimplepgkg

---
+++ simplepkg/src/TNdemo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main() {
+printf("Thank you for partipating in the first stage of the TrustNode workshop.\n");
+printf("Further detaills and sourcecodes will be available after retrieving a license.\n");
+printf("\nSee you next time, your TrustNode-Team.\n");
+}
