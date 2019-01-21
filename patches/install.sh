746446234cfab78d8e9b89e4d950a32019333842
Ulbricht, Marian <ulbricht@innoroute.de>
2019-01-21 17:15:10 +0100

    add_uhttpd_patch

--- patches/install.sh
+++ patches/install.sh
@@ -3,3 +3,5 @@
 #run from makeTrustNodeWRT.sh inside openwrt dir
 #ulbricht@innoroute.de 2018
 cp ../patches/201-fpic.patch  package/libs/openssl/patches/
+mkdir package/network/services/uhttpd/patches
+cp ../patches/002default_src.patch package/network/services/uhttpd/patches/
